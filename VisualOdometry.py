d4197343a3be575af0a3b8783f2d5f142ce13310
Drew Sumsion <drewsumsion10@gmail.com>
2022-04-14 09:40:05 -0600

    testing dynamic threshold

--- VisualOdometry.py
+++ VisualOdometry.py
@@ -35,6 +35,7 @@ class VisualOdometry:
 
         self.intrinsicParameters = np.array(self.intrinsicParameters)
         self.distortionCoefficent = None
+        self.prevFrame = None
 
 
         # template matching parameters
@@ -75,6 +76,89 @@ class VisualOdometry:
 
         return np.array(templates)
 
+    # def templateMatching(self, pathToVid):
+    #     picList = list(glob.glob(pathToVid + "/**.png"))
+    #     picList.sort()
+    #     print(picList)
+    #
+    #     frame = cv.imread(picList[0])
+    #
+    #     saveAt = Path("./Results")
+    #     saveAt.mkdir(exist_ok=True)
+    #     titleName = "task2_"
+    #     cntr = 0
+    #     saveAs = ".avi"
+    #     saveHere = saveAt / (titleName + str(cntr) + saveAs)
+    #     while saveHere.exists():
+    #         cntr += 1
+    #         saveHere = saveAt / (titleName + str(cntr) + saveAs)
+    #
+    #     w = int(frame.shape[0])
+    #     h = int(frame.shape[1])
+    #
+    #     # video recorder
+    #     fourcc = cv.VideoWriter_fourcc(*'XVID')  # cv2.VideoWriter_fourcc() does not exist
+    #
+    #     myRecorder = cv.VideoWriter(str(saveHere), fourcc, 30, (w, h))
+    #
+    #     prevGray_list = []
+    #     prevPoints_list = []
+    #     templates_list = []
+    #     for x in range(self.skipFrames):
+    #         prevGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    #         prevPoints = cv.goodFeaturesToTrack(prevGray, maxCorners=400, qualityLevel=0.25, minDistance=20)
+    #         prevPoints = np.array(prevPoints)
+    #         prevPoints = np.squeeze(prevPoints, axis=1)
+    #         templates = self.makeTemplates(prevPoints, frame)
+    #         prevGray_list.append(prevGray)
+    #         prevPoints_list.append(prevPoints)
+    #         templates_list.append(templates)
+    #
+    #     overall_cntr = 0
+    #     cntr = 0
+    #     for pic in picList:
+    #         frame = cv.imread(pic)
+    #
+    #         frame32 = frame.astype(np.float32)
+    #
+    #         newPoints = []
+    #
+    #         self.cameraPoseMotion(str(pathToVid), overall_cntr, overall_cntr + 1)
+    #         overall_cntr += 1
+    #
+    #         for i in range(len(templates_list[cntr])):
+    #             template = templates_list[0][i]
+    #             oldPoint = prevPoints_list[cntr][i]
+    #
+    #             template = template.astype(np.float32)
+    #             newPoint = self.windowedTemplate(oldPoint, template, frame32.copy())
+    #             newPoints.append(newPoint)
+    #
+    #         newPoints = np.array(newPoints)
+    #
+    #         # draw the tracks
+    #         for i, (new, old) in enumerate(zip(newPoints, prevPoints_list[cntr])):
+    #             if new is None:
+    #                 continue
+    #             a, b = new[0], new[1]
+    #             c, d = old[0], old[1]  #
+    #             frame = cv.line(frame, (int(a), int(b)), (int(c), int(d)), (0, 0, 255), 2)
+    #             frame = cv.circle(frame, (int(a), int(b)), 5, (0, 255, 0), -1)
+    #         cv.imshow("Current Frame", frame)
+    #
+    #         if cv.waitKey(1) & 0xFF == ord('a'):
+    #             break
+    #         myRecorder.write(frame)
+    #
+    #         prevPoints_list[cntr] = newPoints
+    #         # templates_list[cntr] = self.makeTemplates(newPoints, frame)
+    #         cntr += 1
+    #         if cntr >= self.skipFrames:
+    #             cntr = 0
+    #
+    #     # at the end release
+    #     myRecorder.release()
+
     def windowedTemplate(self, oldPoint, template, frame):
         # x_max_orig = frame.shape[0]
         if oldPoint is None:
@@ -113,7 +197,8 @@ class VisualOdometry:
             return None
         return result
 
-    def cameraPoseMotion(self, vidPath, firstFrame, secondFrame):
+    def cameraPoseMotion(self, vidPath, firstFrame, secondFrame, myVid = None, totalVid = None):
+        # print("enters")
         self.firstFrame_index = firstFrame#100#240
         self.secondFrame_index = secondFrame#150#275
 
@@ -122,30 +207,63 @@ class VisualOdometry:
         #
         # ret, frame = myVid.read()
 
-        thePicPath = (glob.glob(vidPath + "/**00" + str(firstFrame) + ".png"))[0]
-        frame = cv.imread(thePicPath)
+        if myVid is not None:
+            # myVid = cv.VideoCapture(vidPath)
+            if self.prevFrame is None:
+                ret, frame = myVid.read()
+                prevGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                # print("prevGray here", prevGray)
+            else:
 
-        cv.imwrite("./Results/task3_frame1.jpg", frame)
+                frame = self.prevFrame
+                prevGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                # print("here", prevGray)
+        else:
+            thePicPath = (glob.glob(vidPath + "/**00" + str(firstFrame) + ".png"))[0]
+            frame = cv.imread(thePicPath)
+            prevGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        # cv.imwrite("./Results/task3_frame1.jpg", frame)
 
         # for x in range(self.skipFrames):
-        prevGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
         prevPoints = cv.goodFeaturesToTrack(prevGray, maxCorners=self.maxCorners, qualityLevel=self.qualitlyLevel, minDistance=self.minDistance)
 
-        thePicPath = (glob.glob(vidPath + "/**00" + str(secondFrame) + ".png"))
-        if len(thePicPath):
-            frame = cv.imread(thePicPath[0])
+        if myVid is not None:
+            # myVid = cv.VideoCapture(vidPath)
+
+            ret, frame = myVid.read()
+            # print(frame.shape)
+            # frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+            if not ret:
+                frame = self.prevFrame
+                nextGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                return None, None
+            else:
+                self.prevFrame = frame.copy()
+                nextGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         else:
-            print("got last frame")
-            frame = cv.imread(glob.glob(vidPath + "/**00" + str(701) + ".png")[0])
+            thePicPath = (glob.glob(vidPath + "/**00" + str(secondFrame) + ".png"))
+            if len(thePicPath):
+                frame = cv.imread(thePicPath[0])
+            else:
+                print("got last frame")
+                frame = cv.imread(glob.glob(vidPath + "/**00" + str(totalVid) + ".png")[0])
         # print(len(thePicPath))
         # frame = cv.imread(thePicPath)
 
-        nextGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            nextGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        # print(prevGray)
+        # print("between")
+        # print(nextGray)
+        # print("before here")
 
         nextPoints, status, error = cv.calcOpticalFlowPyrLK(prevGray, nextGray,
                                                              prevPts=prevPoints, nextPts=None)
-
-        cv.imwrite("./Results/task3_frame2.jpg", frame)
+        # print("after here")
+        # cv.imwrite("./Results/task3_frame2.jpg", frame)
 
         # Select good points
         if nextPoints is not None:
@@ -154,11 +272,25 @@ class VisualOdometry:
         # undistort the points
         new_undistorted = []
         old_undistorted = []
+
+        thisSum = []
+        for i, (new, old) in enumerate(zip(good_new, good_old)):
+            thisSum.append(abs(sum(new-old)))
+
+        thisSum.sort()
+        savePercentile = 0.50
+        cutIndex = int(len(thisSum) * savePercentile)
+        dynamicThresh = thisSum[cutIndex]
+        # print(dynamicThresh)
+
         for i, (new, old) in enumerate(zip(good_new, good_old)):
             # new = cv.undistortPoints(new, self.intrinsicParameters, self.distortionCoefficent)
             # old = cv.undistortPoints(old, self.intrinsicParameters, self.distortionCoefficent)
-            new_undistorted.append(new)
-            old_undistorted.append(old)
+            # print(abs(sum(new - old)))
+            if abs(sum(new-old)) <= dynamicThresh: #pull out noisy ones
+                # print("adding", abs(sum(new - old)))
+                new_undistorted.append(new)
+                old_undistorted.append(old)
 
 
         new_undistorted = np.array(new_undistorted)
@@ -169,12 +301,18 @@ class VisualOdometry:
 
         f, mask = cv.findFundamentalMat(new_undistorted, old_undistorted, cv.FM_8POINT)
         # print("F Matrix: ", f)
-
+        # print("the f", f)
+        # print("new", new_undistorted)
+        # print("old", old_undistorted)
+        # print("the mask", mask)
+        if f is None:
+            return None, None
         E = np.dot(np.dot(np.transpose(self.intrinsicParameters), f), self.intrinsicParameters)
         E = preprocessing.normalize(E)
         # print("Essential Matrix, E:", E)
 
         pts, R, t, mask = cv.recoverPose(E, new_undistorted, old_undistorted)
+        self.displayHelp(new_undistorted, old_undistorted, frame)
 
         # print("Rotation Matrix, R:", R)
         # print("Rotation Matrix, R (3x1):", cv.Rodrigues(R)[0])
@@ -186,6 +324,18 @@ class VisualOdometry:
         self.Translation = t
 
         return t, R
+
+    def displayHelp(self, newPoints, prevPoints, frame):
+        # draw the tracks
+        for i, (new, old) in enumerate(zip(newPoints, prevPoints)):
+            if new is None:
+                continue
+            a, b = new[0], new[1]
+            c, d = old[0], old[1]  #
+            frame = cv.line(frame, (int(a), int(b)), (int(c), int(d)), (0, 0, 255), 2)
+            frame = cv.circle(frame, (int(a), int(b)), 5, (0, 255, 0), -1)
+        cv.imshow("Current Frame", frame)
+        cv.waitKey(1)
 
     def drawGraph(self, allPoints):
         x = []
@@ -202,7 +352,7 @@ class VisualOdometry:
             z_t = point[2][0]
 
             thisX = (x_t / y_t) + prevX
-            thisZ = abs(z_t / y_t) + prevZ
+            thisZ = (z_t / y_t) + prevZ
             x.append(thisX)
             y.append(thisZ)
 
@@ -231,7 +381,7 @@ class VisualOdometry:
 
         plt.figure()
 
-    def calculateAll(self, pathToVid):
+    def calculateAll(self, pathToVid, totalVid):
         saveAt = Path("./Results")
         saveAt.mkdir(exist_ok=True)
         titleName = "R_T_Calculated_" + str(self.skipFrames) + "_" + str(self.maxCorners) + "_" + str(self.qualitlyLevel) + "_" + str(self.minDistance) + "_"
@@ -242,13 +392,24 @@ class VisualOdometry:
             cntr += 1
             saveHere = saveAt / (titleName + str(cntr) + saveAs)
 
+        if ".avi" in pathToVid:
+            myVid = cv.VideoCapture(pathToVid)
+        else:
+            myVid = None
 
         allTranslation = []
         writeList = []
 
-        for overall_cntr in (range(0, 700)):
-            translation, rotation = self.cameraPoseMotion(str(pathToVid), overall_cntr, overall_cntr + self.skipFrames)
-
+        for overall_cntr in (range(0, totalVid)):
+            # print("start")
+            # print(str(pathToVid))
+            # print(overall_cntr)
+            # print(self.skipFrames)
+            # print(myVid)
+            translation, rotation = self.cameraPoseMotion(str(pathToVid), overall_cntr, overall_cntr + self.skipFrames, myVid, totalVid)
+            if translation is None:
+                print("end at ", overall_cntr)
+                break
             saveThis = str(rotation[0][0]) + " " + str(rotation[0][1]) + " " + str(rotation[0][2]) + " "
             saveThis += str(translation[0][0]) + " "
             saveThis += str(rotation[1][0]) + " " + str(rotation[1][1]) + " " + str(rotation[1][2]) + " "
--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import tqdm
 from VisualOdometry import VisualOdometry
 
 if __name__ == "__main__":
-    skipFrames = [2,3,4,5]  # 7#5#3#5 #3
-    maxCorners = [800,1600]
-    qualitlyLevel = [0.01, 0.03, 0.05, 0.07, 0.09]
-    minDistance = [5, 10, 15, 20, 50]
+    # skipFrames = [2,3,4,5]  # 7#5#3#5 #3
+    # maxCorners = [50]#[800, 1600]
+    # qualitlyLevel = [0.1]#[0.01, 0.03, 0.05, 0.07, 0.09]
+    # minDistance = [5, 10, 15, 20, 50]
+    #
+    # for skip in tqdm.tqdm(skipFrames):
+    #     for corner in maxCorners:
+    #         for quality in qualitlyLevel:
+    #             for minDist in minDistance:
+    #                 myVO = VisualOdometry(skip, corner, quality, minDist)
+    #                 myVO.calculateAll("./VO Practice Sequence/VO Practice Sequence/", totalVid = 701)
 
-    for skip in tqdm.tqdm(skipFrames):
-        for corner in maxCorners:
-            for quality in qualitlyLevel:
-                for minDist in minDistance:
-                    myVO = VisualOdometry(skip, corner, quality, minDist)
 
-                    myVO.calculateAll("./VO Practice Sequence/VO Practice Sequence/")
+    doThese = [(2, 800, 0.05, 50), (2, 1600, 0.05, 50), (3, 800, 0.05, 5), (3, 800, 0.09, 10), (3, 1600, 0.09, 10), (4, 800, 0.01, 15), (4, 800, 0.03, 20), (4, 800, 0.05, 10), (4, 800, 0.07, 10), (4, 800, 0.07, 15), (4, 800, 0.07, 20), (4, 1600, 0.07, 10), (4, 1600, 0.07, 15), (4, 1600, 0.07, 20), (5, 800, 0.03, 50), (5, 800, 0.07, 10), (5, 800, 0.07, 15), (5, 800, 0.09, 50), (5, 1600, 0.03, 5), (5, 1600, 0.03, 50), (5, 1600, 0.07, 10), (5, 1600, 0.07, 15), (5, 1600, 0.09, 50)]
+
+    for skip, corner, quality, minDist in tqdm.tqdm(doThese):
+        myVO = VisualOdometry(skip, corner, quality, minDist)
+
+        myVO.calculateAll("./VO Practice Sequence/VO Practice Sequence/", totalVid = 701)
+        # myVO.calculateAll("./data/colorVid.avi")
+        # myVO.calculateAll("./data/vid_0/", totalVid = 208)
